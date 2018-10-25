3af8820d31229ec38a59e5c5f6b6d2c0bc022266
Nathan Golba <ngolba@gmail.com>
2018-10-25 17:27:59 -0400

    Start to survey form

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 
 app.use(bodyParser.json());

f4822364165e5c7af61b5284554256991bfe4adf
Nathan Golba <ngolba@gmail.com>
2018-10-26 04:35:42 -0400

    Working Prototype

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,18 +10,22 @@ fs.readFile(friendPath, 'utf8', (err, data) => {
     friends = JSON.parse(data);
 })
 
-const findMatch  = (user, allUsers) => {
-    let matchScores = allUsers.map(u => (u.scores.map((score, question) => score >= user.scores[question] ? score - user.scores[question]: user.scores[question] - score)).reduce((a, b) => a + b))
-    let match = allUsers[matchScores.indexOf(Math.min(...matchScores))]
-    return match;
+const findMatch = (user, allUsers) => {
+    let nonDuplicateUsers = allUsers.filter(x => (x.name != user.name && x.photo != user.photo ))
+    let matchScores = nonDuplicateUsers
+        .map(u => (u.responses.map((score, question) => score >= user.responses[question] ? score - user.responses[question] : user.responses[question] - score))
+            .reduce((a, b) => a + b))
+    return nonDuplicateUsers[matchScores.indexOf(Math.min(...matchScores))]
 }
 
-// findMatch({user: 'user', scores: [1, 2, 3, 4, 5]}, [{user: 'user2', scores: [2, 2, 2, 2, 2]}, {user: 'user3', scores: [3, 3, 3, 3, 3]}, {user: 'user4', scores: [4, 4, 4, 4, 4]}])
-
 router.post('/api/friends', (req, res) => {
-    friends.push(req.body)
-    fs.writeFile(friendPath, JSON.stringify(friends), 'utf8', err => {if (err) console.log (err)})
-    console.log(friends);
+    let match = findMatch(req.body, friends);
+    friends.push(req.body);
+    fs.writeFile(friendPath, JSON.stringify(friends), 'utf8', err => {
+        if (err) console.log(err)
+    });
+    res.send(match)
+    // console.log(friends);
 })
 
 router.get('/api/friends', (req, res) => {

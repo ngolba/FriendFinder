09be8a10ce7bb422a556f567f1f7eb80cd2f5795
Nathan Golba <ngolba@gmail.com>
2018-10-28 15:54:36 -0400

    Working Beta

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,1 @@
-[{"name":"Nathan","photo":"https://ngolba.github.io/Portfolio/assets/images/headShot.png","responses":["1","4","3","3","5","5","5","4","3","4"]}]
+[{"name":"Nathan","image":"https://ngolba.github.io/Portfolio/assets/images/headShot.png","responses":["1","4","3","3","5","5","5","4","3","4"]}]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,42 +3,54 @@ const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const rimraf = require('rimraf')
 const cloudinary = require('cloudinary');
+require('dotenv').config()
 cloudinary.config({
     cloud_name: 'nihilistff',
     api_key: process.env.cloudinary_key,
     api_secret: process.env.cloudinary_secret
 })
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' });
+const multer = require('multer')
+const upload = multer({
+    dest: 'uploads/'
+});
+const stockImg = 'https://via.placeholder.com/300'
 const friendPath = path.join(__dirname, '../data/friends.js')
-
+let currentUser = {name: '', image: '', responses: []}
 let friends = []
 fs.readFile(friendPath, 'utf8', (err, data) => {
     friends = JSON.parse(data);
 })
 
 const findMatch = (user, allUsers) => {
-    let nonDuplicateUsers = allUsers.filter(x => (x.name != user.name && x.photo != user.photo ))
+    let nonDuplicateUsers = allUsers.filter(x => (x.name != user.name && x.image != user.image))
     let matchScores = nonDuplicateUsers
         .map(u => (u.responses.map((score, question) => score >= user.responses[question] ? score - user.responses[question] : user.responses[question] - score))
             .reduce((a, b) => a + b))
     return nonDuplicateUsers[matchScores.indexOf(Math.min(...matchScores))]
 }
 
-router.post('/api/imageProcessing', upload.single('userUpload'), (req, res) => {
-    console.log(req.file)
-    cloudinary.uploader.upload(req.file.path, (result) => console.log(result))
+router.post('/upload', upload.single('userInfo'), (req, res) => {
+    let { userInfo: [username, userUrl] } = req.body;
+    currentUser.name = username;
+    if (req.file) {
+        cloudinary.uploader.upload(req.file.path, (result) => {
+            currentUser.image = result.url;
+            rimraf(path.join(__dirname, '../../uploads'), () => console.log('upload deleted'))
+        })
+    } else if (userUrl) currentUser.image = userUrl;
+    else currentUser.image = stockImg;
 })
 
 router.post('/api/friends', (req, res) => {
-    let match = findMatch(req.body, friends);
-    friends.push(req.body);
+    currentUser.responses = req.body.responses;
+    let match = findMatch(currentUser, friends);
+    friends.push(currentUser);
     fs.writeFile(friendPath, JSON.stringify(friends), 'utf8', err => {
         if (err) console.log(err)
     });
     res.send(match)
-    // console.log(friends);
 })
 
 router.get('/api/friends', (req, res) => {
